38ce087550d8a8535326ed905ac2ffa4ba3b107a
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-12 10:10:49 +0300

    Initial commit

--- src/Task2_4_6.java
+++ src/Task2_4_6.java
@@ -1,60 +1,26 @@
-import java.util.Map;
 import java.util.Scanner;
 
 // Видеоурок здесь : https://lms.synergy.ru/learning/view/68495/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
 // Задания здесь : https://lms.synergy.ru/learning/view/68557/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
+
+// видеоурок с примером решения здесь: Модуль 1 Урок 2, мин 25.28
 public class Task2_4_6 {
     public static void main(String[] args) {
-        System.out.println("Задание: \n6.\tПользователь вводит строку, выведите ее задом-наперёд.\n\nРешение: ");
+        System.out.println("Задание: \nПользователь вводит строку, выведите ее задом-наперёд.\n\nРешение: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter cyrillic letter, for example 'д': ");
-        String letter = scanner.nextLine();
+        System.out.println("Введите строку: ");
+        String originString = scanner.nextLine();
         System.out.println();
-        System.out.println("Here the word, starting with this letter:");
-        /*Создаем базу данных 'Буква - Слово', в виде списка HashMap*/
-        Map<String, String> map = Map.ofEntries(
-                Map.entry("а",  "альбом"),
-                Map.entry("б",  "бабушка"),
-                Map.entry("в",  "войско"),
-                Map.entry("г",  "горизонт"),
-                Map.entry("д",  "дом"),
-                Map.entry("е",  "еда"),
-                Map.entry("ж",  "жизнь"),
-                Map.entry("з",  "золото"),
-                Map.entry("и",  "икра"),
-                Map.entry("й",  "йод"),
-                Map.entry("к",  "копейка"),
-                Map.entry("л",  "лев"),
-                Map.entry("м",  "мама"),
-                Map.entry("н",  "нос"),
-                Map.entry("о",  "орел"),
-                Map.entry("п",  "песня"),
-                Map.entry("р",  "ребенок"),
-                Map.entry("с",  "слова"),
-                Map.entry("т",  "техника"),
-                Map.entry("у",  "уровень"),
-                Map.entry("ф",  "фарфор"),
-                Map.entry("х",  "характер"),
-                Map.entry("ц",  "цыпленок"),
-                Map.entry("ч",  "чай"),
-                Map.entry("ш",  "шашка"),
-                Map.entry("щ",  "щиколотка"),
-                Map.entry("ъ",  "слово отсутствует"),
-                Map.entry("ы",  "слово отсутствует"),
-                Map.entry("ь",  "слово отсутствует"),
-                Map.entry("э",  "электричество"),
-                Map.entry("ю",  "юбка"),
-                Map.entry("я",  "яблоко")
-        );
+        System.out.println("Строка, перевернутая задом-наперед:");
+        String reversedString = reverse(originString);
+        System.out.println(reversedString);
 
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            String value = entry.getValue();
-            String firstSymbolValue = String.valueOf(value.charAt(0)); // инфо здесь: https://youtu.be/thzfsPEYXfI
-            if (letter.equals(firstSymbolValue)) {
-                System.out.println(value);
-            }
-        }
         System.out.println();
         System.out.println("Done!");
+
+    }
+
+    public static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
